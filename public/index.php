59c33900ba4d7ae9c45da2ee77b25ecb34526e1e
adisalafudin <adiefsal@gmail.com>
2024-07-08 21:17:12 +0700

    initial commmit

--- public/index.php
+++ public/index.php
@@ -3,7 +3,11 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Pc\PlanningEventApp\App\Core\Router;
+use Pc\PlanningEventApp\App\Controllers\HomeController;
 
-Router::add("GET", "/" , "HomeController", "Index");
+Router::add("GET", "/" , HomeController::class , "index");
+Router::add("GET", "/login" , HomeController::class , "hello");
+Router::add("GET", "/register" , HomeController::class , "register");
+
 
 Router::run();
